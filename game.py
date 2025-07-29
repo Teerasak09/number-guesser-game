69f84e0089f74f1c9109d97832bdf63a9e3fd31c
Teerasak09 <teerasakmeebumrung99@gmail.com>
2025-07-29 20:52:19 +0700

    Add initial game template

---
+++ game.py
@@ -0,0 +1,10 @@
+import random
+
+print("🎮 Welcome to the Number Guessing Game!")
+secret = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == secret:
+    print("🎉 Correct! You win!")
+else:
+    print(f"❌ Sorry, the number was {secret}. Better luck next time!")
